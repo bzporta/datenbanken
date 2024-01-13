e9f25c101e551e616ea8ec6ea29d4b040bf71e8f
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2024-01-13 18:20:54 +0100

    Funktionalitäten Patienten

--- public/js/script.js
+++ public/js/script.js
@@ -7,7 +7,7 @@ const loginButton = document.querySelector('form-box.login form button');
 const loginForm = document.querySelector('.form-box.login form');
 const homepageTabs = document.querySelectorAll('.home-page header .tablink');
 const signOutButtons = document.querySelectorAll('.sign-out-button');
-const homepageContents = document.querySelectorAll('.home-page .content-box .content');
+const homepageContents = document.querySelectorAll('.home-page .content-box > .content');
 
 let currentTab = 0;
 
@@ -78,6 +78,16 @@ function showElement(selector) {
     if (element) {
         element.style.display = 'block';
     }
+}
+
+function showElementByIdDisplay(id, display) {
+    /**
+     * Funktion, um das Element anzuzeigen
+     */
+    const element = document.getElementById(id);
+    if (element) {
+        element.style.display = display;
+    } 
 }
 
 // function to show element by id
@@ -201,6 +211,69 @@ treatmentTabButtonOperation.addEventListener('click', (e) => {
     hideElementById('therapietab');
 });
 
+// Patients
+
+aufnahmedatumDatePicker = document.getElementById('patientenaufnahmedatum');
+aufnahmedatumDatePicker.value = new Date().toISOString().slice(0, 10);
+// Createtable Patient
+
+patientCreateTableButton = document.querySelector('#patiententab .wrapper .createTableButton');
+patientCreateTableButton.addEventListener('click', async() => {
+    var result = await executeSqlCommand(
+        `SELECT PATIENTEN_ID, KRANKENHAUS_ID, PATIENTENRAUM_ID, NAME, SUBSTR(AUFNAHME_DATUM ,0,8) AS AUFNAHMEDATUM, SUBSTR(ENTLASSUNGS_DATUM ,0,8) AS ENTLASSUNGSDATUM, GESCHLECHT, SUBSTR(GEBURTSDATUM ,0,8) AS GEBURTSDATUM, BLUTGRUPPE
+        FROM "MIPM"."PATIENT"`
+    )
+    constructTable(result, 'patienten-table');
+});
+
+// Add Patient
+
+addpatientForm = document.getElementById('patienten-form');
+addpatientForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(addpatientForm.patientenentlassungsdatum.value);
+
+    var query_entlassungsdatum = "''";
+    if (addpatientForm.patientenentlassungsdatum.value != '') {
+        query_entlassungsdatum = "TO_DATE('${addpatientForm.patientenentlassungsdatum.value} 00:00:00', 'YYYY-MM-DD HH24:MI:SS')";
+    }
+    const query = `BEGIN
+    INSERT INTO "MIPM"."PATIENT" (KRANKENHAUS_ID, PATIENTENRAUM_ID, NAME, AUFNAHME_DATUM, ENTLASSUNGS_DATUM, GESCHLECHT, GEBURTSDATUM, BLUTGRUPPE) 
+    VALUES (
+                   '${addpatientForm.patientenKrankenhausId.value}',
+                   '${addpatientForm.patientenRaumId.value}',
+                   '${addpatientForm.name.value}',
+                    TO_DATE('${addpatientForm.patientenaufnahmedatum.value} 00:00:00', 'YYYY-MM-DD HH24:MI:SS'),
+                    ${query_entlassungsdatum},
+                   '${addpatientForm.patientengeschlecht.value}',
+                    TO_DATE('${addpatientForm.patientengeburtsdatum.value} 00:00:00', 'YYYY-MM-DD HH24:MI:SS'),
+                   '${addpatientForm.patientenblutgruppe.value}');
+                   COMMIT;
+                   END;`;
+                   
+    console.log(query);
+    executeSqlCommand(query);
+    patientCreateTableButton.click();
+    addpatientForm.reset();
+});
+
+
+// Delete Patient
+
+deletePatientForm = document.querySelector('#patiententab .wrapper form:nth-of-type(2)');
+deletePatientForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const query = `BEGIN
+    DELETE FROM "MIPM"."PATIENT" WHERE PATIENTEN_ID = ${deletePatientForm.patientendelete.value};
+    COMMIT;
+    END;`;
+    
+    executeSqlCommand(query);
+    patientCreateTableButton.click();
+    deletePatientForm.reset();
+});
+
 // Diagnosis
 
 diagnosisdatepicker = document.getElementById('diagnosisdate');
@@ -213,54 +286,113 @@ personnelTabButtonAerzte         = document.getElementById('aerzte-tab-button-2'
 personnelTabButtonKrankenpfleger = document.getElementById('krankenpfleger-tab-button');
 personnelTabButtonAerzte.addEventListener('click', (e) => {
     e.preventDefault();
-    showElementById('aerztetab');
+    showElementByIdDisplay('aerztetab', 'flex');
     hideElementById('krankenpflegertab');
 });
 
 personnelTabButtonKrankenpfleger.addEventListener('click', (e) => {
     e.preventDefault();
-    showElementById('krankenpflegertab');
+    showElementByIdDisplay('krankenpflegertab', 'flex');
     hideElementById('aerztetab');
 });
 
-
-personnelCreateTableButton = document.getElementById('createTableButton');
-personnelCreateTableButton.addEventListener('click', async () => {
+// Createtable Krankenpfleger
+personnelKrankenpflegerCreateTableButton = document.querySelector('#krankenpflegertab .wrapper .createTableButton');
+personnelKrankenpflegerCreateTableButton.addEventListener('click', async() => {
     var result = await executeSqlCommand(
-        `SELECT * FROM "MIPM"."ARZT"
-         `
+        `SELECT * FROM "MIPM"."KRANKENPFLEGER"`
     )
-    constructTable(result, 'Personnel-table');
+    constructTable(result, 'krankenpfleger-table');
 });
 
-personnalkrankenpflegerCreateTableButton = document.getElementById('createTableButton2');
+
+// Personnelform Krankenpfleger
+krankenpflegerForm = document.querySelector('#krankenpflegertab .wrapper form:first-of-type');
+krankenpflegerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const query = `BEGIN
+    INSERT INTO "MIPM"."KRANKENPFLEGER" (KRANKENHAUS_ID, NAME, VORNAME, ARBEITSBEGINN, ARBEITSENDE, QUALIFIKATIONEN, BERECHTIGUNGEN) 
+    VALUES (
+                   '${krankenpflegerForm.krankenhausid.value}',
+                   '${krankenpflegerForm.name.value}',
+                   '${krankenpflegerForm.vorname.value}',
+                   '${krankenpflegerForm.arbeitsbeginn.value}',
+                   '${krankenpflegerForm.arbeitsende.value}',
+                   '${krankenpflegerForm.qualifikationen.value}',
+                   '${krankenpflegerForm.berechtigungen.value}');
+                   COMMIT;
+                   END;`;
+                   
+    executeSqlCommand(query);
+    personnelKrankenpflegerCreateTableButton.click();
+    krankenpflegerForm.reset();
+});
+                
+                // Delete krankenpfleger
+                deleteKrankenpflegerForm = document.querySelector('#krankenpflegertab .wrapper form:nth-of-type(2)');
+                deleteKrankenpflegerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const query = `BEGIN
+    DELETE FROM "MIPM"."KRANKENPFLEGER" WHERE MITARBEITER_NR = ${deleteKrankenpflegerForm.mitarbeiteridDelete.value};
+    COMMIT;
+    END;`;
+    
+    executeSqlCommand(query);
+    personnelKrankenpflegerCreateTableButton.click();
+    deleteKrankenpflegerForm.reset();
+});
 
 // Personnelform ARZT
 personnelForm = document.getElementById('personnel-form');
 personnelForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    
     const query = `BEGIN
-                   INSERT INTO "MIPM"."ARZT" (KRANKENHAUS_ID, NAME, VORNAME, ARBEITSBEGINN, ARBEITSENDE, FACHRICHTUNG, POSITION) 
-                   VALUES (
-                   '${personnelForm.krankenhausid.value}',
-                   '${personnelForm.name.value}',
-                   '${personnelForm.vorname.value}',
-                   '${personnelForm.arbeitsbeginn.value}',
-                   '${personnelForm.arbeitsende.value}',
-                   '${personnelForm.fachrichtung.value}',
-                   '${personnelForm.position.value}');
-                   COMMIT;
+    INSERT INTO "MIPM"."ARZT" (KRANKENHAUS_ID, NAME, VORNAME, ARBEITSBEGINN, ARBEITSENDE, FACHRICHTUNG, POSITION) 
+    VALUES (
+        '${personnelForm.krankenhausid.value}',
+        '${personnelForm.name.value}',
+        '${personnelForm.vorname.value}',
+        '${personnelForm.arbeitsbeginn.value}',
+        '${personnelForm.arbeitsende.value}',
+        '${personnelForm.fachrichtung.value}',
+        '${personnelForm.position.value}');
+        COMMIT;
+        END;`;
+        
+        executeSqlCommand(query);
+        personnelCreateTableButton.click();
+        personnelForm.reset();
+    });
+    
+    // Createtable Arzt
+    personnelCreateTableButton = document.getElementById('createTableButton');
+    personnelCreateTableButton.addEventListener('click', async () => {
+        var result = await executeSqlCommand(
+            `SELECT * FROM "MIPM"."ARZT"
+             `
+        )
+        constructTable(result, 'Personnel-table');
+    });
+
+    // Delete Arzt
+    
+    deleteArztForm = document.querySelector('#aerztetab .wrapper form:nth-of-type(2)');
+    deleteArztForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        const query = `BEGIN
+        DELETE FROM "MIPM"."ARZT" WHERE MITARBEITER_NR = ${deleteArztForm.mitarbeiteridDelete.value};
+        COMMIT;
                    END;`;
 
-    console.log(query);
-    
     executeSqlCommand(query);
-
-    
-
-    personnelForm.reset();
+    personnelCreateTableButton.click();
+    deleteArztForm.reset();
 });
+
 
 
 // Stations
