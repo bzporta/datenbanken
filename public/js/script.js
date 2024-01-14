aee1dc0a2bdefe137887d1e34c670b5a7d5ce8b0
S.Klassen <st4rc4st3r@gmail.com>
2024-01-14 20:21:05 +0100

    Versicherungsgedoens 1

--- public/js/script.js
+++ public/js/script.js
@@ -246,6 +246,254 @@ personnelForm.addEventListener('submit', (e) => {
 
 
 // Patient
+versicherungForm = document.getElementById('versicherung-form-add');
+versicherungForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    const query = `BEGIN
+                   INSERT INTO "MIPM"."VERSICHERUNG" (BETRIEBSNUMMER, VERSICHERUNGSNAME)
+                   VALUES (
+                   '${versicherungForm.betriebsnummer.value}',
+                   '${versicherungForm.versicherungsname.value}');
+                   COMMIT;
+                   END;`;
+
+    console.log(query);
+    
+    executeSqlCommand(query);
+
+    versicherungForm.reset();
+});
+
+patientversicherungForm = document.getElementById('versicherung-form-add-patient');
+patientversicherungForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const query = `BEGIN
+                   INSERT INTO "MIPM"."PATIENT_VERSICHERUNG" (PATIENTEN_ID, BETRIEBSNUMMER, VERSICHERUNGSNUMMER)
+                   VALUES (
+                   '${patientversicherungForm.patientenid.value}',
+                   '${patientversicherungForm.betriebsnummer.value}',
+                   '${patientversicherungForm.versicherungsnummer.value}');
+                   COMMIT;
+                   END;`;
+
+    console.log(query);
+    
+    executeSqlCommand(query);
+
+    patientversicherungForm.reset();
+});
+
+rechnungForm = document.getElementById('rechnung-form-add');
+rechnungForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const query = `BEGIN
+                   INSERT INTO "MIPM"."RECHNUNG" (PATIENTEN_ID, BETRIEBSNUMMER, BETRAG, ZUZAHLUNG, AUSSTELLUNGS_DATUM, FAELLIGKEITS_DATUM, STATUS)
+                   VALUES (
+                   '${rechnungForm.patientenid.value}',
+                   '${rechnungForm.betriebsnummer.value}',
+                   '${rechnungForm.betrag.value}',
+                   '${rechnungForm.zuzahlung.value}',
+                   '${rechnungForm.ausstellungs_datum.value}',
+                   '${rechnungForm.faelligkeits_datum.value}',
+                   1);
+                   COMMIT;
+                   END;`;
+
+    console.log(query);
+    
+    executeSqlCommand(query);
+
+    rechnungForm.reset();
+});
+
+versicherungdelete = document.getElementById('versicherung-form-del');
+versicherungdelete.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const query = `BEGIN
+                   DELETE FROM VERSICHERUNG WHERE BETRIEBSNUMMER = '${versicherungdelete.betriebsnummer.value}';
+                   COMMIT;
+                   END;`;
+
+    console.log(query);
+    
+    executeSqlCommand(query);
+
+    versicherungForm.reset();
+});
+
+versicherungCreateTable = document.getElementById('versicherung-form-read');
+versicherungCreateTable.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    var result;
+    var query = `SELECT * FROM VERSICHERUNG`;
+
+    if (versicherungCreateTable.patientenid.value != '' || versicherungCreateTable.betriebsnummer.value != '' || versicherungCreateTable.versicherungsname.value != '') {
+        if (versicherungCreateTable.patientenid.value != '') {
+            query = `SELECT PATIENT_VERSICHERUNG.PATIENTEN_ID, PATIENT_VERSICHERUNG.BETRIEBSNUMMER, PATIENT_VERSICHERUNG.VERSICHERUNGSNUMMER FROM VERSICHERUNG INNER JOIN PATIENT_VERSICHERUNG ON VERSICHERUNG.BETRIEBSNUMMER = PATIENT_VERSICHERUNG.BETRIEBSNUMMER`;
+        }
+
+        query += ` WHERE`;
+    }
+
+    if (versicherungCreateTable.patientenid.value != '') {
+        query += ` PATIENT_VERSICHERUNG.PATIENTEN_ID = '${versicherungCreateTable.patientenid.value}'`;
+    }
+
+    if (versicherungCreateTable.betriebsnummer.value != '') {
+        if (query.substring(query.length - 5) != 'WHERE') query += ' AND';
+
+        query += ` VERSICHERUNG.BETRIEBSNUMMER = '${versicherungCreateTable.betriebsnummer.value}'`;
+    }
+    
+    if (versicherungCreateTable.versicherungsname.value != '') {
+        if (query.substring(query.length - 5) != 'WHERE') query += ' AND';
+
+        query += ` VERSICHERUNG.VERSICHERUNGSNAME LIKE '${versicherungCreateTable.versicherungsname.value}'`
+    }
+
+    query += ` ORDER BY VERSICHERUNG.BETRIEBSNUMMER ASC`
+
+    result = await executeSqlCommand(query);
+
+    constructTable(result, 'Versicherung-table');
+});
+
+rechnungCreateTable = document.getElementById('rechnung-form-read');
+rechnungCreateTable.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    var result;
+    var query = `SELECT RECHNUNGS_NR, PATIENTEN_ID, BETRIEBSNUMMER, BETRAG, ZUZAHLUNG, AUSSTELLUNGS_DATUM AS AUSSTELLUNGSDATUM, FAELLIGKEITS_DATUM AS FÄLLIGKEITSDATUM, STATUS FROM RECHNUNG`;
+
+    if (rechnungCreateTable.rechnungsnr.value != '' || rechnungCreateTable.patientenid.value != '' || rechnungCreateTable.betriebsnummer.value != '' || rechnungCreateTable.betrag.value != '' || rechnungCreateTable.zuzahlung.value != '' || rechnungCreateTable.ausstellungs_datum.value != '' || rechnungCreateTable.faelligkeits_datum.value != '') {
+        query += ` WHERE`;
+    }
+
+    if (rechnungCreateTable.rechnungsnr.value != '') {
+        if (rechnungCreateTable.rechnungsnr.value.includes('sum')) {
+            query = ` SELECT PATIENTEN_ID, SUM(BETRAG) + SUM(ZUZAHLUNG) AS GESAMTBETRAG FROM RECHNUNG`;
+            if (rechnungCreateTable.patientenid.value != '' || rechnungCreateTable.betriebsnummer.value != '' || rechnungCreateTable.betrag.value != '' || rechnungCreateTable.zuzahlung.value != '') {
+                query += ` WHERE`;
+            }
+        } else if (rechnungCreateTable.rechnungsnr.value.includes('-')) {
+            query += ` RECHNUNGS_NR BETWEEN '${rechnungCreateTable.rechnungsnr.value.substring(0, rechnungCreateTable.rechnungsnr.value.indexOf('-'))}' AND '${rechnungCreateTable.rechnungsnr.value.substring(rechnungCreateTable.rechnungsnr.value.indexOf('-') + 1)}'`;
+        } else {
+            query += ` RECHNUNGS_NR = '${rechnungCreateTable.rechnungsnr.value}'`;
+        }
+    }
+
+    if (rechnungCreateTable.patientenid.value != '') {
+        if (query.substring(query.length - 5) != 'WHERE') query += ' AND';
+
+        if (rechnungCreateTable.patientenid.value.includes('-')) {
+            query += ` PATIENTEN_ID BETWEEN '${rechnungCreateTable.patientenid.value.substring(0, rechnungCreateTable.patientenid.value.indexOf('-'))}' AND '${rechnungCreateTable.patientenid.value.substring(rechnungCreateTable.patientenid.value.indexOf('-') + 1)}'`;
+        } else {
+            query += ` PATIENTEN_ID = '${rechnungCreateTable.patientenid.value}'`;
+        }
+    }
+
+    if (rechnungCreateTable.betriebsnummer.value != '') {
+        if (query.substring(query.length - 5) != 'WHERE') query += ' AND';
+
+        if (rechnungCreateTable.betriebsnummer.value.includes('-')) {
+            query += ` BETRIEBSNUMMER BETWEEN '${rechnungCreateTable.betriebsnummer.value.substring(0, rechnungCreateTable.betriebsnummer.value.indexOf('-'))}' AND '${rechnungCreateTable.betriebsnummer.value.substring(rechnungCreateTable.betriebsnummer.value.indexOf('-') + 1)}'`;
+        } else {
+            query += ` BETRIEBSNUMMER = '${rechnungCreateTable.betriebsnummer.value}'`;
+        }
+    }
+
+    if (rechnungCreateTable.betrag.value != '') {
+        if (query.substring(query.length - 5) != 'WHERE') query += ' AND';
+
+        if (rechnungCreateTable.betrag.value.includes('-', 1)) {
+            query += ` BETRAG BETWEEN '${rechnungCreateTable.betrag.value.substring(0, rechnungCreateTable.betrag.value.indexOf('-'))}' AND '${rechnungCreateTable.betrag.value.substring(rechnungCreateTable.betrag.value.indexOf('-') + 1)}'`;
+        } else {
+            query += ` BETRAG = '${rechnungCreateTable.betrag.value}'`;
+        }
+    }
+
+    if (rechnungCreateTable.zuzahlung.value != '') {
+        if (query.substring(query.length - 5) != 'WHERE') query += ' AND';
+        
+        if (rechnungCreateTable.zuzahlung.value.includes('-', 1)) {
+            query += ` ZUZAHLUNG BETWEEN '${rechnungCreateTable.zuzahlung.value.substring(0, rechnungCreateTable.zuzahlung.value.indexOf('-'))}' AND '${rechnungCreateTable.zuzahlung.value.substring(rechnungCreateTable.zuzahlung.value.indexOf('-') + 1)}'`;
+        } else {
+            query += ` ZUZAHLUNG = '${rechnungCreateTable.zuzahlung.value}'`;
+        }
+    }
+
+    if (rechnungCreateTable.ausstellungs_datum.value != '') {
+        if (query.substring(query.length - 5) != 'WHERE') query += ' AND';
+        
+        if (rechnungCreateTable.ausstellungs_datum.value.includes('-')) {
+            query += ` TRUNC(AUSSTELLUNGS_DATUM) BETWEEN TO_DATE('${rechnungCreateTable.ausstellungs_datum.value.substring(0, rechnungCreateTable.ausstellungs_datum.value.indexOf('-'))}','DD-MM-YYYY') AND TO_DATE('${rechnungCreateTable.ausstellungs_datum.value.substring(rechnungCreateTable.ausstellungs_datum.value.indexOf('-') + 1)}','DD-MM-YYYY')`;
+        } else {
+            query += ` TRUNC(AUSSTELLUNGS_DATUM) = to_date('${rechnungCreateTable.ausstellungs_datum.value}','DD-MM-YYYY')`;
+        }
+    }
+
+    if (rechnungCreateTable.faelligkeits_datum.value != '') {
+        if (query.substring(query.length - 5) != 'WHERE') query += ' AND';
+        
+        if (rechnungCreateTable.faelligkeits_datum.value.includes('fällig')) {
+            query += ` TRUNC(FAELLIGKEITS_DATUM) < TRUNC(SYSDATE) AND STATUS = 1`;
+        } else if (rechnungCreateTable.faelligkeits_datum.value.includes('-')) {
+            query += ` TRUNC(FAELLIGKEITS_DATUM) BETWEEN TO_DATE('${rechnungCreateTable.faelligkeits_datum.value.substring(0, rechnungCreateTable.faelligkeits_datum.value.indexOf('-'))}','DD-MM-YYYY') AND TO_DATE('${rechnungCreateTable.faelligkeits_datum.value.substring(rechnungCreateTable.faelligkeits_datum.value.indexOf('-') + 1)}','DD-MM-YYYY')`;
+        } else {
+            query += ` TRUNC(FAELLIGKEITS_DATUM) = TO_DATE('${rechnungCreateTable.faelligkeits_datum.value}','DD-MM-YYYY')`;
+        }
+    }
+
+    if (rechnungCreateTable.rechnungsnr.value.includes('sum')) {
+        query += ` GROUP BY PATIENTEN_ID`;
+    }
+
+    query += ` ORDER BY PATIENTEN_ID ASC`;
+
+    result = await executeSqlCommand(query);
+
+    constructTable(result, 'Rechnung-table');
+});
+
+patientCreateTableButton = document.getElementById('patient-createTableButton');
+patientCreateTableButton.addEventListener('click', async () => {
+    var result = await executeSqlCommand(
+        `SELECT *
+         FROM PATIENT
+         `
+    )
+    constructTable(result, 'Patient-table');
+
+    var result = await executeSqlCommand( // ATTRIBUTE_6 AS Betriebsnummer, VERSICHERUNGSNAME AS Versicherungsname
+        `SELECT *
+         FROM VERSICHERUNG
+         `
+    )
+    constructTable(result, 'Versicherung-table');
+
+    var result = await executeSqlCommand( // RECHNUNGS_NR, PATIENTEN_ID, ATTRIBUTE_6 AS Betriebsnummer, BETRAG, ZUZAHLUNG
+        `SELECT *
+         FROM RECHNUNG
+         `
+    )
+    constructTable(result, 'Rechnung-table');
+});
+
+executeSqlCommand(`ALTER TABLE VERSICHERUNG
+                   ADD CONSTRAINT FK_PATIENT_VERSICHERUNG
+                   FOREIGN KEY (BETRIEBSNUMMER)
+                   REFERENCES PATIENT_VERSICHERUNG (BETRIEBSNUMMER)
+                   ON DELETE CASCADE`);
+
+executeSqlCommand(`ALTER TABLE VERSICHERUNG
+                   ADD CONSTRAINT FK_VERSICHERUNG_RECHNNG
+                   FOREIGN KEY (BETRIEBSNUMMER)
+                   REFERENCES RECHNUNG (BETRIEBSNUMMER)
+                   ON DELETE CASCADE`);
 
 init();
